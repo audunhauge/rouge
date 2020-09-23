35210da7aed7887a0b2cf3b9daf2f80237d48df6
Audun Hauge <audun.hauge@gmail.com>
2020-09-23 22:34:13 +0200

    v0.02

--- js/rouge.js
+++ js/rouge.js
@@ -1,21 +1,115 @@
 // @ts-check
 
-class Avatar {
-    x = 5;
-    y = 5;
-    div;
+/**
+ * Clamp låser en verdi til området [lo,hi]
+ * Verdier utenfor blir til lo|hi 
+ * @param {number} x
+ * @param {number} lo
+ * @param {number} hi
+ * @returns {number} lo dersom x < lo, hi dersom x > hi, ellers uendra
+ */
+const clamp = (x, lo, hi) => Math.max(lo, Math.min(x, hi));
+let avatar;
+let divActors;
+const monsters = [];
 
+class Actor {
+    x = 2;
+    y = 2;
+    alive = true;
+    /** @type {HTMLElement} */
+    div;
     render() {
         this.div.style.left = (this.x * 32) + "px";
         this.div.style.top = (this.y * 32) + "px";
+        if (!this.alive) {
+            this.div.style.opacity = "0.3";
+            this.div.style.backgroundColor = "rgba(255,0,0,0.5)";
+        }
+    }
+    /**
+     * @param {Actor} other
+     * @returns {number} distance squared
+     */
+    distance(other) {
+        const dx = this.x - other.x;
+        const dy = this.y - other.y;
+        return (dx ** 2 + dy ** 2);
+    }
+    /**
+     * @param {Actor} other
+     * @returns {boolean} true if close
+     */
+    canSee(other) {
+        return this.distance(other) < 25;
+    }
+}
+
+class Monster extends Actor {
+    constructor() {
+        super();
+    }
+
+    action() {
+        if (!this.alive) return;
+        if (this.canSee(avatar)) {
+            if (this.distance(avatar) < 3) {
+                this.attack(avatar);
+            } else {
+                this.towards(avatar);
+            }
+        } else {
+            this.move();
+        }
+    }
+    /**
+     * @param {Actor} other
+     */
+    towards(other) {
+        const dx = other.x - this.x;
+        const dy = other.y - this.y;
+        this.x += clamp(dx, -1, 1);
+        this.y += clamp(dy, -1, 1);
+    }
+
+    /**
+     * @param {Actor} other
+     */
+    attack(other) {
+        if (this.alive && other.alive) {
+            divActors.classList.add("attack");
+            setTimeout(() => divActors.classList.remove("attack"), 300);
+        }
+    }
+    move() {
+        if (!this.alive) return;
+        this.x += Math.round(Math.random() * 2 - 1);
+        this.y += Math.round(Math.random() * 2 - 1);
+        this.x = clamp(this.x, 0, 11);
+        this.y = clamp(this.y, 0, 11);
+    }
+}
+
+class Avatar extends Actor {
+    constructor() {
+        super();
+        this.x = 5;
+        this.y = 5;
     }
 }
 
 
 function setup() {
-    const avatar = new Avatar();
+    avatar = new Avatar();
+    document.querySelectorAll(".monster").forEach(div => {
+        const monster = new Monster();
+        monster.div = /** @type {HTMLElement}*/ (div);
+        monsters.push(monster);
+    })
     avatar.div = document.getElementById("avatar");
+    divActors = document.getElementById("actors");
     avatar.render();
+    monsters.forEach(g => g.render());
 
     document.addEventListener("keydown", doStuff);
 
@@ -27,38 +121,44 @@ function setup() {
         const key = e.key;
         switch (key) {
             case "ArrowLeft":
-                // gjør noe
-                console.log("venstre");
                 if (avatar.x > 0) {
                     avatar.x -= 1;
                     avatar.render();
                 }
                 break;
             case "ArrowRight":
-                // gå til høyre
-                console.log("høyre");
                 if (avatar.x < 11) {
                     avatar.x += 1;
                     avatar.render();
                 }
                 break;
             case "ArrowUp":
-                // gå opp
-                console.log("opp");
                 if (avatar.y > 0) {
                     avatar.y -= 1;
                     avatar.render();
                 }
                 break;
             case "ArrowDown":
-                // gå ned
-                console.log("ned");
                 if (avatar.y < 11) {
                     avatar.y += 1;
                     avatar.render();
                 }
                 break;
+            case "a":
+                // attack something
+                monsters.forEach(g => {
+                    if (g.alive) {
+                        if (g.distance(avatar) < 3) {
+                            g.alive = false;
+                        }
+                    }
+                })
+                break;
 
         }
+        monsters.forEach(goblin => {
+            goblin.action();
+            goblin.render();
+        });
     }
 }
