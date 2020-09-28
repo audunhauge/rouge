98c6de9c25414a1f98188637e688fe3869188e4c
audun hauge <audun.hauge@gmail.com>
2020-09-28 07:46:52 +0200

    tilbakestiller

--- js/rouge.js
+++ js/rouge.js
@@ -1,17 +1,5 @@
 // @ts-check
 
-import  Map  from './mapgen/index.js';
-/*
-import Delaunator from './dual-mesh/node_modules/delaunator';
-let Poisson = require('poisson-disk-sampling');
-let TriangleMesh = require('./');
-let MeshBuilder = require('./create');
-let mesh = new MeshBuilder({boundarySpacing: 450})
-        .addPoisson(Poisson, 450)
-        .create(true);
-*/
-// const map = new Map([],{seed:123,amplitude:3,length:4},() => Math.random()*2-1);
-
 /**
  * Clamp låser en verdi til området [lo,hi]
  * Verdier utenfor blir til lo|hi 
@@ -20,10 +8,8 @@ let mesh = new MeshBuilder({boundarySpacing: 450})
  * @param {number} hi
  * @returns {number} lo dersom x < lo, hi dersom x > hi, ellers uendra
  */
-const clamp = (x, lo, hi) => Math.max(lo, Math.min(x, hi));
-let avatar;
-let divActors;
-const monsters = [];
+
+ let avatar;
 
 class Actor {
     x = 2;
@@ -39,89 +25,14 @@ class Actor {
             this.div.style.backgroundColor = "rgba(255,0,0,0.5)";
         }
     }
-    /**
-     * @param {Actor} other
-     * @returns {number} distance squared
-     */
-    distance(other) {
-        const dx = this.x - other.x;
-        const dy = this.y - other.y;
-        return (dx ** 2 + dy ** 2);
-    }
-    /**
-     * @param {Actor} other
-     * @returns {boolean} true if close
-     */
-    canSee(other) {
-        return this.distance(other) < 25;
-    }
 }
 
-class Monster extends Actor {
-    constructor() {
-        super();
-    }
-
-    action() {
-        if (!this.alive) return;
-        if (this.canSee(avatar)) {
-            if (this.distance(avatar) < 3) {
-                this.attack(avatar);
-            } else {
-                this.towards(avatar);
-            }
-        } else {
-            this.move();
-        }
-    }
-    /**
-     * @param {Actor} other
-     */
-    towards(other) {
-        const dx = other.x - this.x;
-        const dy = other.y - this.y;
-        this.x += clamp(dx, -1, 1);
-        this.y += clamp(dy, -1, 1);
-    }
-
-    /**
-     * @param {Actor} other
-     */
-    attack(other) {
-        if (this.alive && other.alive) {
-            divActors.classList.add("attack");
-            setTimeout(() => divActors.classList.remove("attack"), 300);
-        }
-    }
-    move() {
-        if (!this.alive) return;
-        this.x += Math.round(Math.random() * 2 - 1);
-        this.y += Math.round(Math.random() * 2 - 1);
-        this.x = clamp(this.x, 0, 11);
-        this.y = clamp(this.y, 0, 11);
-    }
-}
-
-class Avatar extends Actor {
-    constructor() {
-        super();
-        this.x = 5;
-        this.y = 5;
-    }
-}
 
 
 export function setup() {
-    avatar = new Avatar();
-    document.querySelectorAll(".monster").forEach(div => {
-        const monster = new Monster();
-        monster.div = /** @type {HTMLElement}*/ (div);
-        monsters.push(monster);
-    })
+    avatar = new Actor();
     avatar.div = document.getElementById("avatar");
-    divActors = document.getElementById("actors");
     avatar.render();
-    monsters.forEach(g => g.render());
 
     document.addEventListener("keydown", doStuff);
 
@@ -156,21 +67,7 @@ export function setup() {
                     avatar.render();
                 }
                 break;
-            case "a":
-                // attack something
-                monsters.forEach(g => {
-                    if (g.alive) {
-                        if (g.distance(avatar) < 3) {
-                            g.alive = false;
-                        }
-                    }
-                })
-                break;
 
         }
-        monsters.forEach(goblin => {
-            goblin.action();
-            goblin.render();
-        });
     }
 }
