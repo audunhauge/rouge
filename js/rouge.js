97b066dc5cc6264037fba62af8512f835e551496
audun hauge <audun.hauge@gmail.com>
2020-09-30 07:38:55 +0200

    refix

--- js/rouge.js
+++ js/rouge.js
@@ -1,118 +1,109 @@
 // @ts-check
 
-
-let avatar;
-
-let monsters = [];
-
-/**
- * Terningfunksjon
- * @param {number} n
- * @returns {number} terning verdi
- */
-function dice(n) {
-  return Math.trunc(Math.random() * n) + 1;
-}
+import { Monster, Player } from "./modules/Monster.js";
+import { Map } from "./modules/Terrain.js";
+import { dice, clamp } from "./modules/util.js";
 
 /**
- * @param {number} x
- * @param {number} lo
- * @param {number} hi
- * @returns {number} lo <= return <= hi
+ * Tar en liste med ids og kjører getById på dem
+ * @param {string} list
+ * @returns {HTMLElement[]}
  */
-function clamp(x, lo, hi) {
-  if (x > hi) return hi;
-  if (x < lo) return lo;
-  return x;
-}
+const getByIds = (list) => {
+  const idList = list.split(",");
+  return idList.map((id) => document.getElementById(id));
+};
 
-class Actor {
-  x = 2;
-  y = 2;
-  /** @type {HTMLElement} */
-  div;
-  constructor() {
-    console.log("Lager en ting");
-  }
-  render() {
-    this.div.style.left = this.x * 32 + "px";
-    this.div.style.top = this.y * 32 + "px";
-    if (!this.alive) {
-      this.div.style.opacity = "0.3";
-      this.div.style.backgroundColor = "rgba(255,0,0,0.5)";
-    }
-  }
-}
-
-class Monster extends Actor {
-  alive = true;
-  constructor() {
-    super();
-  }
-  move() {
-    this.x = clamp(this.x + dice(3) - 2, 0, 11);
-    this.y = clamp(this.y + dice(3) - 2, 0, 11);
-  }
-}
-
-class Player extends Actor {
-  constructor() {
-    super();
-  }
-}
+// dimensjonene på spillebrettet
+const B = { w: 80, h: 80 };
 
 export function setup() {
-  avatar = new Player();
-  avatar.div = document.getElementById("avatar");
-  avatar.render();
+  const [
+    divBoard,
+    divMonsters,
+    divActors,
+    divInfo,
+    divMinimap,
+    divStatus,
+  ] = getByIds("board,monsters,actors,info,minimap,status");
+
+  const brikkeListe = /** @type {HTMLElement[]} */ Array.from(
+    divBoard.querySelectorAll("div")
+  );
 
   document.addEventListener("keydown", doStuff);
 
-  document.querySelectorAll(".monster").forEach((div) => {
-    const m = new Monster();
-    // @ts-ignore
-    m.div = div;
-    m.x = Math.trunc(Math.random() * 11);
-    m.y = Math.trunc(Math.random() * 11);
-    m.render();
-    monsters.push(m);
-  });
+  const { avatar, monsters, map } = startLevel(divMonsters, divActors);
+  map.render(avatar.x-5, avatar.y-5, brikkeListe);
+  avatar.render();
+  enemyAction(monsters,avatar);
 
   /**
    * @param {KeyboardEvent} e
    */
   function doStuff(e) {
+   
     const key = e.key;
     switch (key) {
       case "ArrowLeft":
         if (avatar.x > 0) {
           avatar.x -= 1;
-          avatar.render();
         }
         break;
       case "ArrowRight":
-        if (avatar.x < 11) {
+        if (avatar.x < B.w - 1) {
           avatar.x += 1;
-          avatar.render();
         }
         break;
       case "ArrowUp":
         if (avatar.y > 0) {
           avatar.y -= 1;
-          avatar.render();
         }
         break;
       case "ArrowDown":
-        if (avatar.y < 11) {
+        if (avatar.y < B.h - 1) {
           avatar.y += 1;
-          avatar.render();
         }
         break;
     }
-    for (let i = 0; i < monsters.length; i++) {
-      let m = monsters[i];
-      m.move();
-      m.render();
-    }
+    enemyAction(monsters,avatar);
+    map.render(avatar.x-5, avatar.y-5, brikkeListe);
+    avatar.render();
   }
+}
+
+function enemyAction(monsters,avatar) {
+    const {x,y} = avatar;   // må vite hvor avatar står da kartet centreres på hen
+    const center = {x,y};
+    for (let i = 0; i < monsters.length; i++) {
+        let m = monsters[i];
+        m.move();
+        m.render(center);
+      }
+}
+
+/**
+ * Creates a new level and populates it with monsters
+ * Returns the avatar and monsters array
+ * @param {HTMLElement} divMonsters
+ * @param {HTMLElement} divActors
+ * @returns { { avatar: Player, monsters: Monster[], map: Map }}
+ */
+function startLevel(divMonsters, divActors) {
+  const FIENDS = 4;
+  const avatar = new Player(1, 1);
+  divActors.append(avatar.div);
+  avatar.render();
+
+  const monsters = [];
+  for (let i = 0; i < FIENDS; i++) {
+    const m = new Monster(0, 0);
+    m.x = dice(12) - 1;
+    m.y = dice(12) - 1;
+    monsters.push(m);
+    divMonsters.append(m.div);
+  }
+
+  const map = new Map();
+  return { avatar, monsters, map };
 }
